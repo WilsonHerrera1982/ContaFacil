9125fedd19372b38657d7fc35e2e5abc028dc6b3
Wilson <wherrera.web@gmail.com>
2024-07-09 12:53:51 -0500

    cambios en menu y clase context

--- Models/ContableContext.cs
+++ Models/ContableContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using ContaFacil.Models;
 
 namespace ContaFacil.Models
 {
@@ -9,7 +10,6 @@ namespace ContaFacil.Models
     {
         public ContableContext()
         {
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
 
         public ContableContext(DbContextOptions<ContableContext> options)
@@ -17,21 +17,27 @@ namespace ContaFacil.Models
         {
         }
 
+        public virtual DbSet<CategoriaProducto> CategoriaProductos { get; set; } = null!;
         public virtual DbSet<Cliente> Clientes { get; set; } = null!;
         public virtual DbSet<Cuentum> Cuenta { get; set; } = null!;
         public virtual DbSet<DetalleFactura> DetalleFacturas { get; set; } = null!;
         public virtual DbSet<Empresa> Empresas { get; set; } = null!;
         public virtual DbSet<Factura> Facturas { get; set; } = null!;
+        public virtual DbSet<Impuesto> Impuestos { get; set; } = null!;
+        public virtual DbSet<Inventario> Inventarios { get; set; } = null!;
         public virtual DbSet<Menu> Menus { get; set; } = null!;
         public virtual DbSet<MenuPerfil> MenuPerfils { get; set; } = null!;
         public virtual DbSet<Pago> Pagos { get; set; } = null!;
         public virtual DbSet<Perfil> Perfils { get; set; } = null!;
         public virtual DbSet<Persona> Personas { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+        public virtual DbSet<ProductoProveedor> ProductoProveedors { get; set; } = null!;
         public virtual DbSet<Proveedor> Proveedors { get; set; } = null!;
         public virtual DbSet<TipoPago> TipoPagos { get; set; } = null!;
         public virtual DbSet<TipoTransaccion> TipoTransaccions { get; set; } = null!;
         public virtual DbSet<Tipocuentum> Tipocuenta { get; set; } = null!;
         public virtual DbSet<Transaccion> Transaccions { get; set; } = null!;
+        public virtual DbSet<UnidadMedidum> UnidadMedida { get; set; } = null!;
         public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
         public virtual DbSet<UsuarioPerfil> UsuarioPerfils { get; set; } = null!;
 
@@ -46,6 +52,43 @@ namespace ContaFacil.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<CategoriaProducto>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoriaProducto)
+                    .HasName("categoria_producto_pkey");
+
+                entity.ToTable("categoria_producto");
+
+                entity.Property(e => e.IdCategoriaProducto)
+                    .HasColumnName("id_categoria_producto")
+                    .HasDefaultValueSql("nextval('seq_categoria_producto_id'::regclass)");
+
+                entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+            });
+
             modelBuilder.Entity<Cliente>(entity =>
             {
                 entity.HasKey(e => e.IdCliente)
@@ -299,6 +342,102 @@ namespace ContaFacil.Models
                     .HasConstraintName("factura_id_cliente_fkey");
             });
 
+            modelBuilder.Entity<Impuesto>(entity =>
+            {
+                entity.HasKey(e => e.IdImpuesto)
+                    .HasName("impuesto_pkey");
+
+                entity.ToTable("impuesto");
+
+                entity.Property(e => e.IdImpuesto)
+                    .HasColumnName("id_impuesto")
+                    .HasDefaultValueSql("nextval('seq_impuesto_id'::regclass)");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.Porcentaje)
+                    .HasPrecision(5, 2)
+                    .HasColumnName("porcentaje");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+            });
+
+            modelBuilder.Entity<Inventario>(entity =>
+            {
+                entity.HasKey(e => e.IdInventario)
+                    .HasName("inventario_pkey");
+
+                entity.ToTable("inventario");
+
+                entity.Property(e => e.IdInventario)
+                    .HasColumnName("id_inventario")
+                    .HasDefaultValueSql("nextval('seq_inventario_id'::regclass)");
+
+                entity.Property(e => e.Cantidad)
+                    .HasPrecision(10, 2)
+                    .HasColumnName("cantidad");
+
+                entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaMovimiento)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_movimiento")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.IdProducto).HasColumnName("id_producto");
+
+                entity.Property(e => e.NumeroDespacho)
+                    .HasMaxLength(50)
+                    .HasColumnName("numero_despacho");
+
+                entity.Property(e => e.TipoMovimiento)
+                    .HasMaxLength(1)
+                    .HasColumnName("tipo_movimiento");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.Inventarios)
+                    .HasForeignKey(d => d.IdProducto)
+                    .HasConstraintName("inventario_id_producto_fkey");
+            });
+
             modelBuilder.Entity<Menu>(entity =>
             {
                 entity.HasKey(e => e.IdMenu)
@@ -310,15 +449,17 @@ namespace ContaFacil.Models
                     .HasColumnName("id_menu")
                     .HasDefaultValueSql("nextval('seq_menu'::regclass)");
 
+                entity.Property(e => e.Action)
+                    .HasMaxLength(50)
+                    .HasColumnName("action");
+
+                entity.Property(e => e.Controller)
+                    .HasMaxLength(50)
+                    .HasColumnName("controller");
+
                 entity.Property(e => e.Descripcion)
                     .HasMaxLength(100)
                     .HasColumnName("descripcion");
-                entity.Property(e => e.Controller)
-                   .HasMaxLength(50)
-                   .HasColumnName("controller");
-                entity.Property(e => e.Action)
-                  .HasMaxLength(50)
-                  .HasColumnName("action");
 
                 entity.Property(e => e.Estado)
                     .IsRequired()
@@ -541,6 +682,117 @@ namespace ContaFacil.Models
                     .HasConstraintName("empresa_persona");
             });
 
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.IdProducto)
+                    .HasName("producto_pkey");
+
+                entity.ToTable("producto");
+
+                entity.HasIndex(e => e.Codigo, "producto_codigo_key")
+                    .IsUnique();
+
+                entity.Property(e => e.IdProducto)
+                    .HasColumnName("id_producto")
+                    .HasDefaultValueSql("nextval('seq_producto_id'::regclass)");
+
+                entity.Property(e => e.Codigo)
+                    .HasMaxLength(50)
+                    .HasColumnName("codigo");
+
+                entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.IdCategoriaProducto).HasColumnName("id_categoria_producto");
+
+                entity.Property(e => e.IdUnidadMedida).HasColumnName("id_unidad_medida");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.PrecioUnitario)
+                    .HasPrecision(10, 2)
+                    .HasColumnName("precio_unitario");
+
+                entity.Property(e => e.Stock)
+                    .HasPrecision(10, 2)
+                    .HasColumnName("stock")
+                    .HasDefaultValueSql("0");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+
+                entity.HasOne(d => d.IdCategoriaProductoNavigation)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.IdCategoriaProducto)
+                    .HasConstraintName("producto_id_categoria_producto_fkey");
+
+                entity.HasOne(d => d.IdUnidadMedidaNavigation)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.IdUnidadMedida)
+                    .HasConstraintName("producto_id_unidad_medida_fkey");
+            });
+
+            modelBuilder.Entity<ProductoProveedor>(entity =>
+            {
+                entity.HasKey(e => e.IdProductoProveedor)
+                    .HasName("producto_proveedor_pkey");
+
+                entity.ToTable("producto_proveedor");
+
+                entity.Property(e => e.IdProductoProveedor)
+                    .HasColumnName("id_producto_proveedor")
+                    .HasDefaultValueSql("nextval('seq_producto_proveedor_id'::regclass)");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.IdProducto).HasColumnName("id_producto");
+
+                entity.Property(e => e.IdProveedor).HasColumnName("id_proveedor");
+
+                entity.Property(e => e.PrecioCompra)
+                    .HasPrecision(10, 2)
+                    .HasColumnName("precio_compra");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.ProductoProveedors)
+                    .HasForeignKey(d => d.IdProducto)
+                    .HasConstraintName("producto_proveedor_id_producto_fkey");
+            });
+
             modelBuilder.Entity<Proveedor>(entity =>
             {
                 entity.HasKey(e => e.IdProveedor)
@@ -705,6 +957,45 @@ namespace ContaFacil.Models
                     .HasConstraintName("transaccion_id_tipo_transaccion_fkey");
             });
 
+            modelBuilder.Entity<UnidadMedidum>(entity =>
+            {
+                entity.HasKey(e => e.IdUnidadMedida)
+                    .HasName("unidad_medida_pkey");
+
+                entity.ToTable("unidad_medida");
+
+                entity.Property(e => e.IdUnidadMedida)
+                    .HasColumnName("id_unidad_medida")
+                    .HasDefaultValueSql("nextval('seq_unidad_medida_id'::regclass)");
+
+                entity.Property(e => e.Abreviatura)
+                    .HasMaxLength(10)
+                    .HasColumnName("abreviatura");
+
+                entity.Property(e => e.EstadoBoolean)
+                    .IsRequired()
+                    .HasColumnName("estado_boolean")
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.FechaCreacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_creacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.FechaModificacion)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("fecha_modificacion")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.UsuarioCreacion).HasColumnName("usuario_creacion");
+
+                entity.Property(e => e.UsuarioModificacion).HasColumnName("usuario_modificacion");
+            });
+
             modelBuilder.Entity<Usuario>(entity =>
             {
                 entity.HasKey(e => e.IdUsuario)
@@ -808,6 +1099,8 @@ namespace ContaFacil.Models
                     .HasConstraintName("usuario_perfil_id_usuario_fkey");
             });
 
+            modelBuilder.HasSequence("seq_categoria_producto_id");
+
             modelBuilder.HasSequence("seq_cliente");
 
             modelBuilder.HasSequence("seq_cuenta");
@@ -817,6 +1110,10 @@ namespace ContaFacil.Models
             modelBuilder.HasSequence("seq_empresa");
 
             modelBuilder.HasSequence("seq_factura");
+
+            modelBuilder.HasSequence("seq_impuesto_id");
+
+            modelBuilder.HasSequence("seq_inventario_id");
 
             modelBuilder.HasSequence("seq_menu");
 
@@ -828,6 +1125,10 @@ namespace ContaFacil.Models
 
             modelBuilder.HasSequence("seq_persona");
 
+            modelBuilder.HasSequence("seq_producto_id");
+
+            modelBuilder.HasSequence("seq_producto_proveedor_id");
+
             modelBuilder.HasSequence("seq_proveedor");
 
             modelBuilder.HasSequence("seq_tipo_cuenta");
@@ -837,6 +1138,8 @@ namespace ContaFacil.Models
             modelBuilder.HasSequence("seq_tipo_transaccion");
 
             modelBuilder.HasSequence("seq_transaccion");
+
+            modelBuilder.HasSequence("seq_unidad_medida_id");
 
             modelBuilder.HasSequence("seq_usuario");
 
