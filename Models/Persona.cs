7b5420c49d3b909fc0d88cdca2febd40a75fc549
Wilson <wherrera.web@gmail.com>
2024-07-09 12:30:15 -0500

    restauracion persona

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ContaFacil.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContaFacil.Models
 {
@@ -12,15 +14,25 @@ namespace ContaFacil.Models
         }
 
         public int IdPersona { get; set; }
+        [CustomRequired]
         public string Nombre { get; set; } = null!;
+        [CustomRequired]
         public string? Direccion { get; set; }
+        [CustomRequired]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Este campo debe contener solo dígitos.")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "El número de teléfono debe tener 10 dígitos")]
         public string? Telefono { get; set; }
+        [CustomRequired]
+        [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
         public string? Email { get; set; }
         public bool? Estado { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime? FechaModificacion { get; set; }
         public int UsuarioCreacion { get; set; }
         public int? UsuarioModificacion { get; set; }
+        [CustomRequired]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Este campo debe contener solo dígitos.")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "El número de identificación debe tener 10 dígitos")]
         public string? Identificacion { get; set; }
         public int? IdEmpresa { get; set; }
 
